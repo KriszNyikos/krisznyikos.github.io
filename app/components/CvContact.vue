4355105cd06719d2d74d8f7840a59ed4dbd15965
Krisztian Nyikos <krisz.nyikos@gmail.com>
2026-06-18 18:34:26 +0200

    feat: refactor CvContact component layout to use flexbox and improve styling

--- app/components/CvContact.vue
+++ app/components/CvContact.vue
@@ -3,35 +3,40 @@ import type { CvContent } from '@/types'
 
 const { contact } = defineProps<{
   contact: CvContent['contact']
-  title: string
 }>()
 </script>
 
 <template>
-  <ul class="list-none p-0 m-0 grid gap-1">
-    <li>
-      <strong>Email:</strong> <a
-        class="text-blue-600 hover:underline"
-        :href="`mailto:${contact.email}`"
-      >{{ contact.email }}</a>
-    </li>
-    <li><strong>Phone:</strong> {{ contact.phone }}</li>
-    <li><strong>Location:</strong> {{ contact.location }}</li>
-    <li>
-      <strong>LinkedIn:</strong> <a
-        class="text-blue-600 hover:underline"
-        :href="`https://${contact.linkedin}`"
-        target="_blank"
-        rel="noreferrer"
-      >{{ contact.linkedin }}</a>
-    </li>
-    <li>
-      <strong>Github:</strong> <a
-        class="text-blue-600 hover:underline"
-        :href="`https://${contact.github}`"
-        target="_blank"
-        rel="noreferrer"
-      >{{ contact.github }}</a>
-    </li>
-  </ul>
+  <div class="flex flex-row gap-4 flex-wrap text-sm">
+    <div
+      class="flex flex-row gap-1"
+    >
+      <UIcon
+        name="i-lucide-mail"
+        class="size-5"
+      /> {{ contact.email }}
+    </div>
+
+    <div
+      class="flex flex-row gap-1"
+    >
+      <UIcon
+        name="i-lucide-phone"
+        class="size-5"
+      /> {{ contact.phone }}
+    </div>
+
+    <div
+      class="flex flex-row gap-1"
+    >
+      <UIcon
+        name="i-lucide-map-pin"
+        class="size-5"
+      /> {{ contact.location }}
+    </div>
+
+    <div
+      class="flex flex-row gap-1"
+    />
+  </div>
 </template>
